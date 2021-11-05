fbddf4657c22eb5a7d7f068a5ab82bc1ad35fd5c
mybonus <506832550@qq.com>
2021-11-06 07:59:46 +0800

    complete without google OAuth due to the proxy issue

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const mongoose = require("mongoose");
 const session = require('express-session');
 const passport = require('passport');
 const passportLocalMongoose = require('passport-local-mongoose');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const findOrCreate = require('mongoose-findorcreate');
+const HttpsProxyAgent = require('https-proxy-agent');
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
@@ -33,10 +36,13 @@ mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
 
 const userSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    googleId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 
 
 // const secret = "Thisisourlittlesecret.";
@@ -47,12 +53,49 @@ const User = new mongoose.model("User", userSchema);
 
 passport.use(User.createStrategy()); // create a local log in strategy.
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+const gStrategy = new GoogleStrategy({
+        clientID: process.env.CLIENT_ID,
+        clientSecret: process.env.CLIENT_SECRET,
+        callbackURL: "http://localhost:3000/auth/google/secrets",
+        userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+    },
+    function(accessToken, refreshToken, profile, done) {
+        User.findOrCreate({ googleId: profile.id }, function(err, user) {
+            return done(err, user);
+        });
+    }
+);
+
+passport.use(gStrategy);
+
+const agent = new HttpsProxyAgent(process.env.HTTP_PROXY || "http://192.168.23.4:999");
+gStrategy._oauth2.setAgent(agent);
+
+passport.use(gStrategy);
 
 app.get("/", function(req, res) {
     res.render("home");
 });
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['https://www.googleapis.com/auth/plus.login'] })
+);
+
+app.get('/auth/google/secrets',
+    passport.authenticate('google', { failureRedirect: '/login' }),
+    function(req, res) {
+        res.redirect('/secrets');
+    });
 
 app.get("/register", function(req, res) {
     res.render("register");
@@ -63,14 +106,41 @@ app.get("/login", function(req, res) {
 });
 
 app.get("/secrets", function(req, res) {
-    //console.log(req);
-    console.log(req.isAuthenticated());
+    User.find({ "secret": { $ne: null } }, function(err, foundUsers) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUsers) {
+                res.render("secrets", { usersWithSecrets: foundUsers });
+            }
+        }
+    });
+});
+
+app.get("/submit", function(req, res) {
     if (req.isAuthenticated()) {
-        res.render("secrets");
-        console.log("@");
+        console.log("@@")
+        res.render("submit");
     } else {
         res.redirect("/login");
     }
+});
+
+app.post("/submit", function(req, res) {
+    const submittedSecret = req.body.secret;
+    //console.log(req.user);
+    User.findById(req.user._id, function(err, foundUser) {
+        if (err) {
+            console.log(err);
+        } else {
+            if (foundUser) {
+                foundUser.secret = submittedSecret;
+                foundUser.save(function() {
+                    res.redirect("/secrets");
+                });
+            }
+        }
+    });
 });
 
 app.post("/register", function(req, res) {
